c3a3d06021660aa42dc5139daf1987b151a823ef
mohamed-islam1 <mohsanaa710@gmail.com>
2025-05-25 14:29:19 +0200

    feat: Update price for Athletic Tank Top to 5000

--- scripts.js
+++ scripts.js
@@ -235,7 +235,7 @@ const products = [
     {
         id: 7,
         name: "Athletic Tank Top",
-        price: 39.99,
+        price: 5000,
         img: "https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1026&q=80",
         description: "Ultra-breathable for intense workouts",
         available: 50,
